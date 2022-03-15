b8e6b470155b4e1eae90c824c9ff7b9898e4b2e0
Parkjju <rudwns3927@gmail.com>
2022-03-15 23:52:16 +0900

    Feature: 인풋 엔터 키 이벤트 핸들러 등록 및 submit기능 추가

--- project/static/js/btn-slide.js
+++ project/static/js/btn-slide.js
@@ -1,9 +1,17 @@
 const btn = document.getElementById('start-btn');
+const input = document.querySelector('.hide');
 
 function slideInput() {
     if (this.firstChild.nodeType == 3) {
-        this.firstElementChild.className = 'visible';
+        this.firstElementChild.firstElementChild.className = 'visible';
         this.removeChild(this.firstChild);
     }
 }
+
+function submitName(e) {
+    if (e.keyCode == 13) {
+        login.submit();
+    }
+}
 btn.addEventListener('click', slideInput);
+input.addEventListener('keydown', submitName);
